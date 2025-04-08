97c5f9430f87f4c97b7afc9dcfb67d4c57e9e8b2
sahil <sahil06butala@gmail.com>
2025-04-08 16:21:48 +0530

    adding cloudinary storage

---
+++ backend/controllers/Cloudinary/index.js
@@ -0,0 +1,21 @@
+// config/cloudinary.js
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv'
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+dotenv.config()
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'upload',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+  },
+});
+
+export default cloudinary;
--- backend/routes/auth/authRoute.js
+++ backend/routes/auth/authRoute.js
@@ -1,28 +1,29 @@
-import express from 'express'
-import { getUserInfo, RegisterUser, UserLogin } from '../../controllers/auth/authController.js'
-import { AuthMiddleWare } from '../../middlewares/authMiddleWare.js'
-import upload from '../../middlewares/Multer.js'
+import express from "express";
+import {
+  getUserInfo,
+  RegisterUser,
+  UserLogin,
+} from "../../controllers/auth/authController.js";
+import { AuthMiddleWare } from "../../middlewares/authMiddleWare.js";
+import { storage } from "../../controllers/Cloudinary/index.js";
+import multer from "multer";
 
-const authRouter = express.Router()
+const authRouter = express.Router();
+const upload = multer({ storage });
 
 // all routes
 
-authRouter.post('/register',RegisterUser)
-authRouter.post('/login',UserLogin)
-authRouter.post('/upload-image',upload.single('image'),(req,res)=>{
-       if(!req.file){
-        return res.status(400).json({
-             message  : "no file uploaded"
-        })
-       }
+authRouter.post("/register", RegisterUser);
+authRouter.post("/login", UserLogin);
+authRouter.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({
+      message: "no file uploaded",
+    });
+  }
 
-       const imageUrl  = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
+  res.status(200).json({ imageUrl: req.file.path });
+});
+authRouter.get("/getUser", AuthMiddleWare, getUserInfo);
 
-       res.status(200).json({imageUrl})
-})
-authRouter.get('/getUser',AuthMiddleWare,getUserInfo)
-
-
-
-
-export default authRouter
+export default authRouter;
